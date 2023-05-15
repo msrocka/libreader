6810413db7301b5538cdcd95f62a7335eec66329
msrocka <michael.srocka@gmail.com>
2023-05-15 16:33:14 +0200

    add a CachingLibReader

--- src/main/java/org/openlca/libreader/DirectLibReader.java
+++ src/main/java/org/openlca/libreader/DirectLibReader.java
@@ -1,51 +1,83 @@
 package org.openlca.libreader;
 
+import org.openlca.core.database.IDatabase;
 import org.openlca.core.library.LibMatrix;
 import org.openlca.core.library.Library;
 import org.openlca.core.matrix.format.MatrixReader;
 import org.openlca.core.matrix.index.EnviIndex;
 import org.openlca.core.matrix.index.ImpactIndex;
 import org.openlca.core.matrix.index.TechIndex;
+import org.openlca.npy.Array2d;
 
+/**
+ * A library reader that directly reads the data elements
+ * from files on each method call without caching anything.
+ */
 public class DirectLibReader implements LibReader {
+
+	private final IDatabase db;
+	private final Library lib;
+
+	private DirectLibReader(Library lib, IDatabase db) {
+		this.lib = lib;
+		this.db = db;
+	}
+
+	public static LibReader of(Library lib, IDatabase db) {
+		return new DirectLibReader(lib, db);
+	}
 
 	@Override
 	public Library library() {
-		return null;
+		return lib;
 	}
 
 	@Override
 	public TechIndex techIndex() {
-		return null;
+		return lib.syncTechIndex(db).orElse(null);
 	}
 
 	@Override
 	public EnviIndex enviIndex() {
-		return null;
+		return lib.syncEnviIndex(db).orElse(null);
 	}
 
 	@Override
 	public ImpactIndex impactIndex() {
-		return null;
+		return lib.syncImpactIndex(db).orElse(null);
 	}
 
 	@Override
-	public MatrixReader matrixOf(LibMatrix matrix) {
-		return null;
+	public MatrixReader matrixOf(LibMatrix m) {
+		return lib.getMatrix(m).orElse(null);
 	}
 
 	@Override
 	public double[] costs() {
-		return new double[0];
+		return lib.getCosts().orElse(null);
 	}
 
 	@Override
-	public double[] diagonalOf(LibMatrix matrix) {
-		return new double[0];
+	public double[] diagonalOf(LibMatrix m) {
+		var f = MatrixFile.of(lib, m);
+		if (f.isEmpty())
+			return null;
+		if (f.hasMatrix())
+			return f.matrix().diag();
+		return Array2d.readDiag(f.file())
+			.asDoubleArray()
+			.data();
 	}
 
 	@Override
-	public double[] columnOf(LibMatrix matrix, int col) {
-		return new double[0];
+	public double[] columnOf(LibMatrix m, int j) {
+		var f = MatrixFile.of(lib, m);
+		if (f.isEmpty())
+			return null;
+		if (f.hasMatrix())
+			return f.matrix().getColumn(j);
+		return Array2d.readColumn(f.file(), j)
+			.asDoubleArray()
+			.data();
 	}
 }
--- src/main/java/org/openlca/libreader/MatrixCache.java
+++ src/main/java/org/openlca/libreader/MatrixCache.java
@@ -1,7 +1,6 @@
 package org.openlca.libreader;
 
 import java.util.EnumMap;
-import java.util.function.Supplier;
 
 import gnu.trove.map.hash.TIntObjectHashMap;
 import org.openlca.core.library.LibMatrix;
@@ -39,13 +38,8 @@ class MatrixCache {
 		return diag;
 	}
 
-	double[] diagonalOf(LibMatrix m, Supplier<double[]> fn) {
-		var cached = diagonalOf(m);
-		if (cached != null)
-			return cached;
-		var diag = fn.get();
+	void putDiagonal(LibMatrix m, double[] diag) {
 		diagonals.put(m, diag);
-		return diag;
 	}
 
 	double[] columnOf(LibMatrix m, int j) {
@@ -58,17 +52,6 @@ class MatrixCache {
 		if (matrix == null)
 			return null;
 		var column = matrix.getColumn(j);
-		cache.put(j, column);
-		return column;
-	}
-
-	double[] columnOf(LibMatrix m, int j, Supplier<double[]> fn) {
-		var cached = columnOf(m, j);
-		if (cached != null)
-			return cached;
-		var column = fn.get();
-		var cache = columns.computeIfAbsent(
-			m, $ -> new TIntObjectHashMap<>());
 		cache.put(j, column);
 		return column;
 	}
